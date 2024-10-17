6fedc4f2b0d623cedb8baeff04a6f015af5d8ba6
Gina Vasiloff <ginavasiloff@gmail.com>
2024-10-17 12:25:18 -0600

    Add basic movement
    Co-authored-by: Alex Stuart <alex@das.li>

---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(moveHorizontal, moveVertical, 0) * moveSpeed * Time.deltaTime;
+    }
+}
